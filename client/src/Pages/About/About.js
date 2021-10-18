f15411fe2615acdfd4c64bf30ce29d933412f968
Tolga Secme <tolgasecme@icloud.com>
2021-10-18 15:20:35 -0600

    navbar margin setup

--- client/src/Pages/About/About.js
+++ client/src/Pages/About/About.js
@@ -7,7 +7,6 @@ export default function About() {
                 <div className="row justify-content-center">
                     <div className="col-lg-9 text-center">
                         <h2 className="text-center mt-0 about">About Golf and Bounce</h2>
-                        <hr className="divider my-4" color="#408eff" />
                         <p>Located in Flatiron Malls</p>
                     </div>
                     <img src={PriceList} className="d-block w-100 rounded mb-3" alt="priceList" />
--- client/src/Pages/Home/Home.js
+++ client/src/Pages/Home/Home.js
@@ -9,9 +9,7 @@ export default function Home() {
                 <div className="row justify-content-center">
                     <div className="col-lg-9 text-center">
                         <h2 className="text-center mt-0 about text-danger">We Are Open!</h2>
-                        <hr className="divider my-4" color="#408eff" />
                         <a href="/golfandbounce" className="grow btn btn-primary">Book a Birthday Party!</a>
-                        <hr className="divider my-4" color="#408eff" />
                     </div>
                 </div>
             </div>
